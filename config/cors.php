ab64aded2c76394073537cb648b44ea5e4af2b0f
randahmad1998 <randahmad1998@gmail.com>
2026-07-18 08:59:44 +0300

    update base url

--- config/cors.php
+++ config/cors.php
@@ -18,6 +18,7 @@ return [
         'http://localhost:5173',
         'http://localhost:3000',
         'http://127.0.0.1:5173',
+        'https://sales.phoenitech.sy',
     ],
 
     'allowed_origins_patterns' => [],
